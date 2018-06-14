8aeb15f8dca714a86ebbff58408afd993c1bebe6
Ben <Ben@DESKTOP-P9J5P21>
2018-06-14 22:31:31 +0200

    Added Eventlistener

--- src/score/ScoreManager.java
+++ src/score/ScoreManager.java
@@ -1,10 +1,31 @@
 package score;
 
+import com.betbot.main.Main;
+import com.betbot.wm.Match;
+import com.betbot.wm.MatchEvent;
+
 public class ScoreManager {
+	public void init(){
+		Main.getWMManager().addMatchEvent(new MatchEvent(){
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			@Override
+			public void PreMatch(Match match) {
+				// TODO Auto-generated method stub
+				
+			}
 
+			@Override
+			public void MatchStarted(Match match) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void MatchFinished(Match match) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 	}
-
 }
