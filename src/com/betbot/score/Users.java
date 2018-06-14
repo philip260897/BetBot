c1e87c47eb82240bb91d33a1f8ac97b3ed4d8299
Ben <Ben@DESKTOP-P9J5P21>
2018-06-14 23:13:27 +0200

    Users Added

---
+++ src/com/betbot/score/Users.java
@@ -0,0 +1,32 @@
+package com.betbot.score;
+
+public class Users {
+	private String username;
+	private int score;
+	private int tip1;
+	private int tip2;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setScore(int score) {
+		this.score = score;
+	}
+	public int getTip1() {
+		return tip1;
+	}
+	public void setTip1(int tip) {
+		this.tip1 = tip;
+	}
+	public int getTip2() {
+		return tip2;
+	}
+	public void setTip2(int tip2) {
+		this.tip2 = tip2;
+	}
+}
