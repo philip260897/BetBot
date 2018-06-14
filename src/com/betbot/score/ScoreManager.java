9b65f87ebb4bd432f5603d6934dfe76d82e382c5
Ben <Ben@DESKTOP-P9J5P21>
2018-06-15 00:10:51 +0200

    User creating

--- src/com/betbot/score/ScoreManager.java
+++ src/com/betbot/score/ScoreManager.java
@@ -1,5 +1,10 @@
 package com.betbot.score;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import com.betbot.bot.TelegramBotEvent;
 import com.betbot.main.Logger;
 import com.betbot.main.Main;
@@ -7,6 +12,8 @@ import com.betbot.wm.Match;
 import com.betbot.wm.MatchEvent;
 
 public class ScoreManager {
+	static int i = 0;
+	static List<Users> users = new ArrayList<Users>();
 	public static void init(){
 		Main.getWMManager().addMatchEvent(new MatchEvent(){
 
@@ -31,23 +38,35 @@ public class ScoreManager {
 		Main.getTelegramBot().addTelegramBotEvent(new TelegramBotEvent(){
 
 			@Override
-			public void MessageReceived(String message, String sender) {
+			public void MessageReceived(String message, String sender, long chatId) {
 				// TODO Auto-generated method stub
 
 			}
 
 			@Override
-			public void CommandReceived(String cmd, String[] args, String sender) {
+			public void CommandReceived(String cmd, String[] args, String sender, long chatId) {
 				// TODO Auto-generated method stub
+				
 				if(cmd.equalsIgnoreCase("register")){
 					Users user = new Users();
 					user.setUsername(sender);
-					user.setScore(0);
+					user.setScore(i++);
 					user.setTip1(0);
 					user.setTip2(0);
 					Logger.LogResult("Registred");
+					users.add(user);
 				}
-				//if(cmd.equalsIgnoreCase("tip1"));
+				if(cmd.equalsIgnoreCase("getscore")){
+					Logger.LogResult("Registred");
+					for(Users s : users) {
+						   if(s.getUsername().contains(sender)) {
+						            Logger.Log(""+s.getScore());
+						   }
+						}
+					
+				}
+				
+				
 			}
 			
 		});
